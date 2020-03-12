980a329237517614279ffe659e1975df56fbbfe3
Al3xDo <56794124+Al3xDo@users.noreply.github.com>
2020-03-12 11:18:28 +0700

    third commit

--- XuatKho.py
+++ XuatKho.py
@@ -4,27 +4,33 @@ import os
 import re
 import datetime
 # set up file
+dirname = os.path.join(os.getcwd(), 'fileXuat.txt')
+if not os.path.exists():
+    os.makedirs('dirname')
+os.chdir(dirname)
+# tạo và viết cookies
 '''dirname = os.path.join(os.getcwd(), 'ToolExcel')
 os.chdir(dirname)'''
 wb = openpyxl.load_workbook('Xuat.xlsx')
 sheet = wb.get_sheet_by_name('Sheet1')
 # viet regex
 kihieu = re.compile(r'''
-^(\w\d{2,4}?) # ma so
+^(\w\d{2,4}) # ma so
 (\s|,)
-(\w{5}) # size
+(\w{5,7}) # size
 (\s|,)
 (.*?) # mau
 (\s|,)
-(\d) # so luong
+(\d{1,3}) # so luong
 ''', re.VERBOSE)
 # thu nhap cookies
+# add nhanh cac cot
 # giao dien
 print('Lưu ý save excel trước khi dùng')
 print('Lưu ý gõ ko dấu')
 print('1.c để quét nội dung (can copy vao clipboard truoc)')
 print('2.z để quét nội dung trong notepad')
-print('3.` để thoát')
+print('3.`thoát')
 while True:
     # kiem tra dau vao
     x = input()
@@ -32,8 +38,8 @@ while True:
         break
     # chuc nang 1
     elif x == 'c':
-        test = pyperclip.paste()
         maxcol = sheet.max_column
+        test = pyperclip.paste()
         test = pyperclip.paste()
         n = kihieu.search(test)
         if n == None:
@@ -50,7 +56,7 @@ while True:
             while c <= maxcol:
                 if size == sheet.cell(row=1, column=c).value:
                     while c <= maxcol:
-                        if mau == sheet.cell(row=2, column=c).value:
+                        if mau == str(sheet.cell(row=2, column=c).value).strip():
                             excol = c
                             c = maxcol+1
                             size = ' '
@@ -70,8 +76,10 @@ while True:
                         print('Đã chỉnh xong')
                 else:
                     r = r+1
+        print('Hoàn tất')
 # chuc nang 2
     elif x == 'z':
+        maxcol = sheet.max_column
         test = open('ma.txt')
         test1 = test.readlines()
         for t in test1:
@@ -112,7 +120,21 @@ while True:
                     else:
                         r = r+1
         test.close()
+        print('Hoàn tất')
 # cuoi
-fdate = datetime.date.today().strftime('%d/%m/%Y')
-wb.save('Xuat'+fdate+'.xlsx')
-print('Đã save với tên Xuat'+fdate+'.xlsx')
+print('Ấn 1 để lưu')
+print('Ấn 2 để ko lưu')
+x = input()
+if x == '1':
+    print('Nhập tên của file lưu, bỏ trống sẽ định dạng theo tên Xuat Ngay Thang)
+    name = input()
+    print('...')
+    if name == '' or ' ':
+        today = datetime.date.today()
+        day = today.day
+        month = today.month
+        name = str(day)+'-'+str(month)
+    else:
+        wb.save('Xuat '+name+' .xlsx')
+        print('Đã save với tên Xuat '+name+'.xlsx')
+print('Đã hoàn tất')
